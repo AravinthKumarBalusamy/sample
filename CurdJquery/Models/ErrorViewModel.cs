ac2e471ea57ee29367964ef09d6453acbf49c2a4
Aravinth kumar <aravinth726@gmail.com>
2020-06-07 19:13:44 +0530

    Project created

---
+++ CurdJquery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurdJquery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
